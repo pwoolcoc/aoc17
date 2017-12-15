481f151d46197d2c817053a2ee7b8def1d2b7106
Paul Woolcock <pwoolcock@tenable.com>
2017-12-15 06:10:44 -0500

    second half of day 15

--- fifteen/src/lib.rs
+++ fifteen/src/lib.rs
@@ -6,13 +6,15 @@ const B_FACTOR: u64 = 48271;
 struct Generator {
     valu: u64,
     factor: u64,
+    criteria: u64,
 }
 
 impl Generator {
-    fn new(value: u64, factor: u64) -> Generator {
+    fn new(value: u64, factor: u64, criteria: u64) -> Generator {
         Generator {
             valu: value,
             factor: factor,
+            criteria: criteria,
         }
     }
 }
@@ -21,9 +23,13 @@ impl Iterator for Generator {
     type Item = u64;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let interm = self.valu * self.factor;
-        self.valu = interm % NUM;
-        Some(self.valu)
+        Some(loop {
+            let interm = self.valu * self.factor;
+            self.valu = interm % NUM;
+            if self.valu % self.criteria == 0 {
+                break self.valu
+            }
+        })
     }
 }
 
@@ -31,9 +37,9 @@ fn low16bits(num: u64) -> u64 {
     num & 0xffff
 }
 
-fn run(astart: u64, bstart: u64, rounds: usize) -> usize {
-    let a = Generator::new(astart, A_FACTOR);
-    let b = Generator::new(bstart, B_FACTOR);
+fn run(astart: u64, acrit: u64, bstart: u64, bcrit: u64, rounds: usize) -> usize {
+    let a = Generator::new(astart, A_FACTOR, acrit);
+    let b = Generator::new(bstart, B_FACTOR, bcrit);
     let iter = a.zip(b);
     let count = iter.take(rounds).map(|(a, b)| (low16bits(a), low16bits(b))).filter(|&(a, b)| a == b).count();
     count
@@ -45,11 +51,11 @@ mod tests {
 
     #[test]
     fn it_works() {
-        assert_eq!(run(65, 8921, 40_000_000), 588);
+        assert_eq!(run(65, 4, 8921, 8, 5_000_000), 309);
     }
 
     #[test]
     fn answer() {
-        println!("answer is {}", run(591, 393, 40_000_000));
+        println!("answer is {}", run(591, 4, 393, 8, 5_000_000));
     }
 }
